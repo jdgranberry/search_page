8e7ac8ae87849bec12aa7aba32a2ad510def4fe5
Josh Granberry <jdgranberry@gmail.com>
2017-03-23 19:23:51 -0500

    Initial working version with ES

--- web/search_page.py
+++ web/search_page.py
@@ -3,11 +3,14 @@ import configparser
 import urllib.parse
 import urllib.request
 import json
+import certifi
+from elasticsearch import Elasticsearch
+
 
 
 app = Flask(__name__)
 
-NUM_SEARCH_RESULTS = 10         # Valid values are integers between 1 and 10, inclusive.
+NUM_SEARCH_RESULTS = 10
 
 # Pull in the API key to enable search
 config = configparser.ConfigParser()
@@ -15,30 +18,46 @@ config.read('search_page.env')
 api_key = config['Google Search API']['Apikey']
 cx = config['Google Search API']['Cx']
 
+# Elasticsearch server
+es_host = config['Elasticsearch Server']['Host']
+es_port = config['Elasticsearch Server']['Port']
+es_user = config['Elasticsearch Server']['User']
+es_pass = config['Elasticsearch Server']['Pass']
+
+es = Elasticsearch([es_host], http_auth=(es_user, es_pass), port=es_port)
+
+ES_INDEX = 'song_lyrics'
+
 
 @app.route('/')
 def hello_world():
-    return render_template('search.html', my_string="Hello World!", my_list=[0, 1, 2, 3, 4, 5])
+    return render_template('websearch.html', my_string="Hello World!", my_list=[0, 1, 2, 3, 4, 5])
 
 
 @app.route("/home")
 def home():
-    return render_template('search.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5], title="Home")
+    return render_template('websearch.html', my_string="Wheeeee!", my_list=[0, 1, 2, 3, 4, 5], title="Home")
 
 
 @app.route("/about")
 def about():
-    return render_template('search.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5], title="About")
+    return render_template('websearch.html', my_string="Wheeeee!", my_list=[0, 1, 2, 3, 4, 5], title="About")
 
 
 @app.route("/contact")
 def contact():
-    return render_template('search.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5], title="Contact Us")
+    return render_template('websearch.html', my_string="Wheeeee!", my_list=[0, 1, 2, 3, 4, 5], title="Contact Us")
 
 
-@app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form['search terms']
+# Web Search Functionality #########################################################################
+@app.route("/websearch")
+def websearch():
+    return render_template('websearch.html', my_string="Wheeeee!", my_list=[0, 1, 2, 3, 4, 5], title="Home")
+
+
+@app.route('/api/websearch', methods=['POST'])
+def websearch_post():
+    text = request.form['websearch']
 
     params = {'key': api_key,
               'cx': cx,
@@ -46,18 +65,33 @@ def my_form_post():
               'num': NUM_SEARCH_RESULTS}
 
     url = 'https://www.googleapis.com/customsearch/v1?%s' % urllib.parse.urlencode(params)
-    # with urllib.request.urlopen(url) as f:
-    #     json_data = json.loads(f.read().decode('utf-8'))
+    with urllib.request.urlopen(url) as f:
+        json_data = json.loads(f.read().decode('utf-8'))
 
-    # DEBUG CODE
-    import os
-    with open(os.path.abspath('/home/jdgranberry/Dropbox/College/CS4315 - Intro Data Mining/assignment_3/search_page/web/test_search.json'), 'r') as testdata:
-        json_data = json.loads(testdata.read())
-
-    return render_template('web_search_results.html',
+    return render_template('websearch_results.html',
                            query=text,
                            length=len(json_data['items']),
                            results=json_data['items'])
+
+
+# Elasticsearch Functionality#######################################################################
+@app.route("/elasticsearch")
+def elasticsearch():
+    return render_template('elasticsearch.html', my_string="Wheeeee!", my_list=[0, 1, 2, 3, 4, 5], title="About")
+
+
+@app.route("/api/elasticsearch", methods=["POST"])
+def elasticsearch_post():
+    text = request.form['elasticsearch']
+
+    res = es.search(index=ES_INDEX, size=50, body={"query": {"match_all": {}}})
+
+    results = ""
+    for hit in res['hits']['hits']:
+        artist = hit['_source']['Artist'].__str__()
+        song = hit['_source']['Song'].__str__()
+        results += "{}, {}<div>".format(artist, song)
+    return results
 
 if __name__ == '__main__':
     app.run()
